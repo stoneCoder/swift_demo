21f95a1f962c8cc8da2140bdcc83c0f04a67e133
stoneCoder <>
2020-09-21 09:26:25 +0800

    第三方库添加

---
+++ SwiftDemo/SwiftDemo/SwiftDemo.swift
@@ -0,0 +1,106 @@
+//
+//  SwiftDemo.swift
+//  SwiftDemo
+//
+//  Created by sinosun on 2020/9/21.
+//  Copyright © 2020 sinosun. All rights reserved.
+//
+
+import UIKit
+
+class SwiftDemo: NSObject {
+    func test()  {
+        var age:Int
+        age = 28
+        print("your age is \(age)")
+        let name = "Sir" //常量用let
+        var otherName:String;
+        otherName = "Thanks"
+        print("Great work \(otherName) \(name)")
+        var feeling = "awesome"
+        feeling = "Great"
+        print("Your mood is \(feeling) !")
+        let secondsPerMinute = 60
+        print(secondsPerMinute)
+        let appName = "Code Swift"
+        let review = "is a fantastic app!";
+        let message = appName + review;
+        print(message)
+        let quote = "Practice makes perfect!";
+        print(quote)
+        let language = "Swift"
+        print("I am learning \(language)")
+        var company = "Apple"
+        let yearFounded = 1976
+        var str = " was founded in "
+        print(company + str + String(yearFounded)) //字符串拼接才需要转换
+        let celsius:Float
+        let fahrenheit:Float = 50
+        celsius = (fahrenheit - 32)/1.8
+        print("It is \(celsius) degrees Celuius.")
+        /*Bool 类型*/
+        var isAwesome = true
+        print(isAwesome)
+        let number1 = 9
+        let number2 = 3
+        let iAgree = number1 > number2
+        print(iAgree)
+        let iAmFun = true
+        let youAreFun = true
+        let weAreFun = iAmFun != youAreFun
+        print(weAreFun)
+        let trueOrFalse = number1 != number2
+        print(trueOrFalse)
+        var player1Won = true
+        var player2Won = !player1Won
+        print(player2Won)
+        let locked = true
+        let alarmOn = true
+        var doorSecure = locked && alarmOn //与运算
+        print(doorSecure)
+        var walked = false
+        var biked = true
+        var walkedOrBiked = walked || biked //或运算
+        print(walkedOrBiked)
+        let hisAge = 33
+        let herAge = 33
+        let sameAge = hisAge == herAge
+        print(sameAge)
+        var doorIsOpen = !locked
+        print(doorIsOpen)
+        var bluetoothEnabled:Bool
+        var isFit = true
+        var injured = false
+        var readyToPlay = isFit && !injured
+        print(readyToPlay)
+        
+        var months = 12
+        var month : String = "October"
+        let totalApples = 15
+        let totalOranges : Int = 5
+        var totalFruits = totalApples + totalOranges
+        print(totalFruits)
+        let price : Double = 3.47
+        let tax : Int = 1
+        var total = price + Double(tax)
+        print(total)
+        let weekend : Bool = false
+        
+        if weekend {
+            print("Woohoo, let's party!")
+        } else {
+            print("Need to set my alarm")
+        }
+        
+        /*数组*/
+        var monthTotle = (7,"July")
+        print("Month number is \(monthTotle.0)")
+        print("Month name is \(monthTotle.1)")
+        
+        /*如果变量或常量可能根本不存储任何值，则可以在Swift中使用可选类型声明它。 这是通过在类型后面添加问号（？）来完成的。
+         例如，如果变量maysName可能不包含任何值，则应使用String类型声明它？ 与String相对。*/
+        var favoriteAnimal : String? = "Penguin"
+        favoriteAnimal = nil
+        print(favoriteAnimal)
+    }
+}
